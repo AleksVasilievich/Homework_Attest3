940ed01565d7f8b5e7df9589256e1ee163f0f531
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2023-03-26 22:42:34 +0300

    Add sketch Function

---
+++ Notes/Function.py
@@ -0,0 +1,10 @@
+class Function:
+    def input_notes(self):
+        print('save_notes')
+
+    def reads_notes(self):
+        print('reads_notes')
+    def deletes(self):
+        print('deletes')
+    def view_id(self):
+        print("view_id")
